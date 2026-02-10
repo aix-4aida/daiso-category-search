81b2448bd6b12e5e40427ff498dc39f1463f69c9
MINSsss96 <minsue0422@naver.com>
2026-02-10 15:04:42 +0900

    일단 배포 먼저 해보기#3 https://github.com/aix-4aida/daiso-category-search/tree/feature/kms 배포

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     // Only recognize .ts and .tsx files for pages, ignoring .jsx files in src/pages
     // This effectively disables Pages Router and uses only App Router
     pageExtensions: ['ts', 'tsx'],
+    output: 'standalone',
 };
 
 module.exports = nextConfig;
