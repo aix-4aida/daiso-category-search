caf23bf85fe416461355188865ff919f5baf5a66
DevPeng <pengveloper1127@gmail.com>
2026-02-11 09:51:33 +0900

    chore: DB만 남기고 리셋

--- backend/api/api.py
+++
@@ -1,8 +0,0 @@
-
-from fastapi import APIRouter
-from backend.api.endpoints import stt, search, categories
-
-api_router = APIRouter()
-api_router.include_router(stt.router, prefix="/stt", tags=["stt"])
-api_router.include_router(search.router, prefix="/search", tags=["search"])
-api_router.include_router(categories.router, tags=["categories"])
